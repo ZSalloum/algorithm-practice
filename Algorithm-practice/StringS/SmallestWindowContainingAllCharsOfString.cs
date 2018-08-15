8d0be5e547f6aada609f43fb49288938abba072d
Ziad SALLOUM <zsalloum@hotmail.com>
2018-08-15 16:54:34 +0200

    Add tests

--- Algorithm-practice/StringS/SmallestWindowContainingAllCharsOfString.cs
+++ Algorithm-practice/StringS/SmallestWindowContainingAllCharsOfString.cs
@@ -7,7 +7,7 @@ namespace AlgorithmPractice.Strings
 
     /**
      * https://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
-     * 
+     
      * Find the smallest window in a string containing all characters of another string
         Given two strings string1 and string2, find the smallest substring in string1 containing all characters of string2 efficiently.
         For Example:
@@ -133,6 +133,16 @@ namespace AlgorithmPractice.Strings
 
             String result = item.FindSubString(str, pat);
             Assert.True(result == "cc");
+        }
+
+        [Test]
+        public void Should_Return_Empty()
+        {
+            String str = "abcdecfcc";
+            String pat = "xyz";
+
+            String result = item.FindSubString(str, pat);
+            Assert.True(result == "");
         }
     }
 }
